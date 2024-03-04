566bf4d28f78e03a4da2f1d10f061a35f8cbabf9
cyxigo <cyxigo@bk.ru>
2024-03-04 20:48:54 +0300

    bottom buttons component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import TitleComponent from "./components/TitleComponent";
 import ToDoListComponent from "./components/ToDoListComponent";
+import ButtonsComponent from "./components/ButtonsComponent";
 
 function App() {
   return (
     <>
       <TitleComponent />
+      <ButtonsComponent />
       <ToDoListComponent />
     </>
   );
---
+++ src/components/ButtonsComponent/index.tsx
@@ -0,0 +1,36 @@
+import { Component } from "solid-js";
+
+import styles from "./style.module.css";
+import { setCookie } from "../utils/cookie";
+
+const ButtonsComponent: Component = () => {
+  function handleDeleteClick() {
+    if (
+      confirm(
+        "Are you sure you want to delete all cookies? This action will delete all your todos and cannot be undone."
+      )
+    ) {
+      setCookie("todos", "");
+      location.reload();
+    }
+  }
+
+  function handleGitClick() {
+    open("https://github.com/cyxigo/todo");
+  }
+
+  return (
+    <div class={styles.wrapper}>
+      <button class={styles.buttonDelete} onClick={handleDeleteClick}>
+        {" "}
+        Delete cookies{" "}
+      </button>
+      <button class={styles.buttonGit} onClick={handleGitClick}>
+        {" "}
+        GitHub{" "}
+      </button>
+    </div>
+  );
+};
+
+export default ButtonsComponent;
